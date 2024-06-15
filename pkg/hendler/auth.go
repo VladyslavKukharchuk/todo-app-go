383af762f4e05069adfb95002f8fbdb420d213c7
Кухарчук Владислав Віталійович <v.kukharchuk@cyberpolice.gov.ua>
2024-06-15 12:32:56 +0300

    Add endpoints

---
+++ pkg/hendler/auth.go
@@ -0,0 +1,11 @@
+package hendler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) singUp(c *gin.Context) {
+
+}
+
+func (h *Handler) singIn(c *gin.Context) {
+
+}
---
+++ todo.go
@@ -0,0 +1,26 @@
+package todo
+
+type TodoList struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserList struct {
+	Id     int `json:"id"`
+	UserId int `json:"userId"`
+	ListId int `json:"listId"`
+}
+
+type TodoItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type ListItem struct {
+	Id     int `json:"id"`
+	ListId int `json:"listId"`
+	ItemId int `json:"itemId"`
+}
